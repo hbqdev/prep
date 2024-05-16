b11b0f3735bc309af4d5ecb7b3e7202c1a1134aa
Tin Tran <hbq.dev@gmail.com>
2024-05-15 21:27:06 -0700

    cog push

--- simple.py
+++ simple.py
@@ -9,16 +9,23 @@ load_dotenv()
 REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
 
 #model_version = "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478"
-model_version="meta/meta-llama-3-8b"
+#model_version="meta/meta-llama-3-8b"
 
 # Ask the user for a prompt
-prompt = input("Please enter a prompt for the model: ")
+#prompt = input("Please enter a prompt for the model: ")
 
 # Define the input data
-input_data = {"prompt": prompt}
+#input_data = {"prompt": prompt}
 
 # Run the model
-output = replicate.run(model_version, input=input_data)
+#output = replicate.run(model_version, input=input_data)
 
+def run_tin_test():
+    model_file= "hbqdev/tin-test:d65a7a69b84d17f12d7c47654b6073f433ce71b31e67f7e359a7ea61303529cd"
+    input_data = {"image": open("output.jpg", "rb")}
+    output = replicate.run(model_file, input=input_data)
+    print(output)
 # Print the output URL
-print(output[0])
+#print(output[0])
+
+run_tin_test()
